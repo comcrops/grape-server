b45ebbfe14a2cb2c494b39942d2b684c641b7bdb
David Hain <d.hain@gmx.at>
2024-01-17 01:31:37 +0100

    feat: database connection with sqlx and .env file

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 
 ## Setup
 
-The `ROCKET_DATABASES` environment variable has to be set to connect to the postgres database.\
-See [here](https://api.rocket.rs/v0.5/rocket_db_pools/index.html#configuration) for further info on that.
+Do not forget to setup your .env file.
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use rocket::response::content;
 use rocket::serde::json::Json;
 use rocket::serde::Deserialize;
-use rocket_db_pools::{
-    sqlx::{self},
-    Connection, Database,
-};
+use rocket::State;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::{Pool, Postgres};
+use url::Url;
 
 #[macro_use]
 extern crate rocket;
@@ -19,34 +19,52 @@ struct AddData {
     expires_at: Option<i32>,
 }
 
-#[derive(Database)]
-#[database("grape")]
-struct DbPool(sqlx::Pool<sqlx::Postgres>);
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    dotenv::dotenv().expect("Failed to load .env file");
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build()
-        .attach(DbPool::init())
+    let db_url = format!(
+        "postgres://{}:{}/{}",
+        dotenv::var("DB_HOST").expect("Failed to get DB_HOST"),
+        dotenv::var("DB_PORT").expect("Failed to get DB_PORT"),
+        dotenv::var("DB_NAME").expect("Failed to get DB_NAME")
+    );
+    let mut db_url = Url::parse(&db_url).expect("Failed to parse database URL");
+    db_url
+        .set_username(&dotenv::var("DB_USER").expect("Failed to get DB_USER"))
+        .expect("Failed to set username");
+    db_url
+        .set_password(Some(&dotenv::var("DB_PWD").expect("Failed to get DB_PWD")))
+        .expect("Failed to set password");
+
+    let db_pool = PgPoolOptions::new()
+        .connect(db_url.as_str())
+        .await
+        .expect("Failed to connect to database");
+
+    let _rocket = rocket::build()
+        .manage(db_pool)
         .mount("/api/v1", routes![add, get])
+        .launch()
+        .await?;
+
+    Ok(())
 }
 
 #[post("/add", data = "<body>")]
-async fn add(body: Json<AddData>, mut db: Connection<DbPool>) -> content::RawHtml<String> {
+async fn add(body: Json<AddData>, db: &State<Pool<Postgres>>) -> content::RawHtml<String> {
     let body = body.0;
 
     dbg!(&body);
 
-    let a = sqlx::query(
-        r#"INSERT INTO grape (text, password, url, expires_at) VALUES ($1, $2, $3, $4)"#,
-    )
-    .bind(body.text)
-    .bind(body.password)
-    .bind(body.url)
-    .bind(body.expires_at)
-    .execute(&mut **db)
-    .await;
-
-    dbg!(a);
+    sqlx::query("INSERT INTO grape (text, password, url, expires_at) VALUES ($1, $2, $3, $4)")
+        .bind(&body.text)
+        .bind(&body.password)
+        .bind(&body.url)
+        .bind(&body.expires_at)
+        .execute(&**db)
+        .await
+        .expect("Failed to insert into database");
 
     todo!()
 }
