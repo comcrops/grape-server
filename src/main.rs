de9d88f56ae863be0b0b9b42e605f2a2e25bbb4a
s-prechtl <stefan@tague.at>
2024-01-18 07:40:49 +0100

    feat: ackshual encryption

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
+use aes_gcm::aead::generic_array::GenericArray;
+use aes_gcm::aead::{Aead, OsRng};
+use aes_gcm::{AeadCore, Aes256Gcm, Key, KeyInit};
 use rocket::response::content;
 use rocket::serde::json::Json;
 use rocket::serde::Deserialize;
 use rocket::time::PrimitiveDateTime;
 use rocket::State;
+use sha2::{Digest, Sha256};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::{Pool, Postgres};
 use time::OffsetDateTime;
-use sha2::{Sha256, Digest};
 
 #[macro_use]
 extern crate rocket;
@@ -55,14 +58,22 @@ async fn add(body: Json<AddData>, db: &State<Pool<Postgres>>) -> content::RawHtm
         None => uuid::Uuid::new_v4().to_string(),
     };
 
+    let mut nonce: Option<Vec<u8>> = None;
+    let mut text = body.text.as_bytes().to_vec();
+
+    if body.password.is_some() {
+        (text, nonce) = encrypt_text(&body.text, &body.password.clone().unwrap());
+    }
+
     let password = hash_password(body.password);
 
     sqlx::query!(
-        r#"INSERT INTO paste (text, password, url, expires_at) VALUES ($1, $2, $3, $4)"#,
-        body.text,
+        r#"INSERT INTO paste (text, password, url, expires_at, nonce) VALUES ($1, $2, $3, $4, $5)"#,
+        text,
         password,
         url,
-        expiring_date
+        expiring_date,
+        nonce
     )
     .execute(&**db)
     .await
@@ -78,7 +89,22 @@ async fn get(url: &str, db: &State<Pool<Postgres>>) -> content::RawHtml<String> 
         .await
         .expect("Failed to fetch from database");
 
-    content::RawHtml(paste.text)
+    match String::from_utf8(paste.text) {
+        Ok(text) => content::RawHtml(text),
+        Err(err) => content::RawHtml(err.to_string()),
+    }
+}
+
+fn encrypt_text(text: &str, password: &str) -> (Vec<u8>, Option<Vec<u8>>) {
+    let key = Key::<Aes256Gcm>::from_slice(password.as_bytes());
+    let cipher = Aes256Gcm::new(&key);
+    let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+    (
+        cipher
+            .encrypt(&nonce, text.as_bytes())
+            .expect("Can't fail since we don't use payload"),
+        Some(nonce.to_vec()),
+    )
 }
 
 fn hash_password(password: Option<String>) -> Option<String> {
